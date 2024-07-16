d793f5a7b871353de1f888c69189c0795cf7370d
ytrufchev <yordan.tihomirov@icloud.com>
2024-07-16 16:40:55 +0300

    Setup connection to database for dev and prod profiles

---
+++ src/main/java/com/lms/learning_management_system/User.java
@@ -0,0 +1,23 @@
+package com.lms.learning_management_system;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+
+    // getters and setters
+}
