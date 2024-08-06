103d738b0cef99669e914317f259119703f175f8
ytrufchev <yordan.tihomirov@gmail.com>
2024-08-06 14:22:52 +0300

    Added UserControllerTest

--- src/test/java/com/lms/learning_management_system/ControllerTest/UserControllerTest.java
+++ src/test/java/com/lms/learning_management_system/ControllerTest/UserControllerTest.java
@@ -5,19 +5,12 @@ import com.lms.learning_management_system.dto.UserDTO;
 import com.lms.learning_management_system.entities.RoleEnum;
 import com.lms.learning_management_system.entities.service.UserService;
 import com.lms.learning_management_system.exception.UserNotFoundException;
-import com.lms.learning_management_system.exception.UserAlreadyExistsException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
@@ -27,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UserController.class)
-public class UserControllerTest {
+class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -36,7 +29,7 @@ public class UserControllerTest {
     private UserService userService;
 
     @Test
-    public void testCreateUser() throws Exception {
+    void testCreateUser() throws Exception {
         UserDTO userDTO = new UserDTO(UUID.randomUUID(), "John", "Doe", "john.doe@example.com", RoleEnum.STUDENT.toString());
         when(userService.createUser(any(UserDTO.class))).thenReturn(userDTO);
 
@@ -51,7 +44,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testUpdateUser() throws Exception {
+    void testUpdateUser() throws Exception {
         UUID userId = UUID.randomUUID();
         UserDTO userDTO = new UserDTO(userId, "John", "Doe", "john.doe@example.com", RoleEnum.STUDENT.toString());
         when(userService.updateUser(eq(userId), any(UserDTO.class))).thenReturn(userDTO);
@@ -67,7 +60,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testGetUser() throws Exception {
+    void testGetUser() throws Exception {
         UUID userId = UUID.randomUUID();
         UserDTO userDTO = new UserDTO(userId, "John", "Doe", "john.doe@example.com", RoleEnum.STUDENT.toString());
         when(userService.getUserById(userId)).thenReturn(userDTO);
@@ -82,7 +75,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testGetUserNotFound() throws Exception {
+    void testGetUserNotFound() throws Exception {
         UUID userId = UUID.randomUUID();
         when(userService.getUserById(userId)).thenThrow(new UserNotFoundException("User with Id " + userId + " Not found"));
 
@@ -91,7 +84,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testUpdateUserNotFound() throws Exception {
+    void testUpdateUserNotFound() throws Exception {
         UUID userId = UUID.randomUUID();
         when(userService.updateUser(eq(userId), any(UserDTO.class))).thenThrow(new UserNotFoundException("User with Id " + userId + " Not found"));
 
@@ -102,7 +95,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testDeleteUser() throws Exception {
+    void testDeleteUser() throws Exception {
         UUID userId = UUID.randomUUID();
         doNothing().when(userService).deleteUser(userId);
 
@@ -111,7 +104,7 @@ public class UserControllerTest {
     }
 
     @Test
-    public void testDeleteUserNotFound() throws Exception {
+    void testDeleteUserNotFound() throws Exception {
         UUID userId = UUID.randomUUID();
         doThrow(new UserNotFoundException("User with Id " + userId + " Not found")).when(userService).deleteUser(userId);
 
